54984cc0396264e6373565665cde6a479d62bb0a
NnoromIV <nnorom.prince44@gmail.com>
2022-09-12 14:52:56 +0100

    Refactoring

--- ge-web/src/components/PrimaryHeader.js
+++ ge-web/src/components/PrimaryHeader.js
@@ -12,12 +12,34 @@ const PrimaryHeader = () => {
       </div>
       <div className="PrimaryHeader_Search">
       <Form inline="true" >
-        <FormControl placeholder='Search' className='PrimaryHeader_Search_Text' type='text'/>
+        <FormControl placeholder='Search skills, experts'  className='PrimaryHeader_Search_Text' type='text'/>
         <Button type='submit' variant='primary' className='PrimaryHeader_Search_Logo'>
         <FontAwesomeIcon icon={faSearch} size='md' color='white'/>
         </Button>
       </Form>
       </div>
+      <div className="PrimaryHeader_Links">
+      <ul>
+        <li>
+          <a href="/"> Skill</a>
+        </li>
+        <li>
+          <a href="/"> Experts</a>
+        </li>
+        <li>
+          <a href="/"> Why us?</a>
+        </li>
+        <li>
+          <a href="/"> About us</a>
+        </li>
+      </ul>
+      </div>
+      <Button className='PrimaryHeader_Links_Log_In'>
+        LOG IN
+      </Button>
+      <Button className='PrimaryHeader_Links_Sign_In'>
+        SIGN IN
+      </Button>
     </div>
   )
 }
