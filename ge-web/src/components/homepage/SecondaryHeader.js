1b8f1dc21f79661bbddb602a98f12ca2f81cad61
NnoromIV <nnorom.prince44@gmail.com>
2022-09-12 16:38:44 +0100

    Secondary header

---
+++ ge-web/src/components/homepage/SecondaryHeader.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import '../styles/SecondaryHeader.css'
+import {Button} from 'react-bootstrap'
+
+const Jobs = [
+    {
+        name: 'Domestic chore'
+    },
+    {
+        name: 'Web Development'
+    },
+    {
+        name: 'Copy-Writing'
+    },
+    {
+        name: 'Voice over'
+    },
+    {
+        name: 'Design'
+    },
+    {
+        name: 'Mobile Development'
+    },
+    {
+        name: 'Contract'
+    },
+    {
+        name: 'Finance Manager'
+    }
+]
+
+const SecondaryHeader = () => {
+  return (
+    <div className='SecondaryHeader_Body'>
+        <div className='SecondaryHeader_Body_Jobs'>
+        {
+            Jobs.map(job => {
+                return <h2 className='SecondaryHeader_Jobs'>
+                    {job.name}
+                </h2>
+            })
+        }
+        </div>
+        <div className="SecondaryHeader_All_Categories">
+            <Button type='button'>ALL CATEGORIES</Button>
+        </div>
+    </div>
+  )
+}
+
+export default SecondaryHeader
--- ge-web/src/pages/Homepage.js
+++ ge-web/src/pages/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import PrimaryHeader from '../components/homepage/PrimaryHeader'
+import SecondaryHeader from '../components/homepage/SecondaryHeader'
 import './styles/Homepage.css'
 
 const Homepage = () => {
   return (
     <div className='Homepage_Body'>
       <PrimaryHeader />
+      <SecondaryHeader />
     </div>
   )
 }
