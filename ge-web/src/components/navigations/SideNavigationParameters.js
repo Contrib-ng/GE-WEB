3920faaba0bddedc03a32b9c8f3e0d32959bb9db
NnoromIV <nnorom.prince44@gmail.com>
2022-09-26 13:26:40 +0100

    Page in production page

--- ge-web/src/components/navigations/SideNavigationParameters.js
+++ ge-web/src/components/navigations/SideNavigationParameters.js
@@ -1,3 +1,4 @@
+import PageInProduction from "../../pages/PageInProduction"
 import { ExpertDashboard, ExpertProfile } from "../accountpage/ExpertAccountPage"
 
 export const PLACEHOLDER_IMG = 'https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
@@ -23,35 +24,35 @@ export const EXPERTNAVIGATION = [
         inactiveImage: 'https://img.icons8.com/external-inkubators-basic-outline-inkubators/344/external-empty-folder-file-and-folder-inkubators-basic-outline-inkubators.png',
         activeImage: 'https://img.icons8.com/external-inkubators-basic-outline-inkubators/344/fb5012/external-empty-folder-file-and-folder-inkubators-basic-outline-inkubators.png',
         navigationName: 'Projects',
-        link: ''
+        link: <PageInProduction />
     },
     {
         index: 'messages',
         inactiveImage: 'https://img.icons8.com/external-outline-stroke-bomsymbols-/2x/external-chat-communication-outline-outline-stroke-bomsymbols--6.png',
         activeImage: 'https://img.icons8.com/external-outline-stroke-bomsymbols-/2x/fb5012/external-chat-communication-outline-outline-stroke-bomsymbols--6.png',
         navigationName: 'Messages',
-        link: ''
+        link: <PageInProduction />
     },
     {
         index: 'calendar',
         inactiveImage: 'https://img.icons8.com/external-line-adri-ansyah/2x/external-essentials-essentials-ui-line-adri-ansyah-39.png',
         activeImage: 'https://img.icons8.com/external-line-adri-ansyah/2x/fb5012/external-essentials-essentials-ui-line-adri-ansyah-39.png',
         navigationName: 'Calender',
-        link: ''
+        link: <PageInProduction />
     },
     {
         index: 'help',
         inactiveImage: 'https://img.icons8.com/external-line-adri-ansyah/2x/external-social-social-media-interface-line-adri-ansyah-4.png',
         activeImage: 'https://img.icons8.com/external-line-adri-ansyah/2x/fb5012/external-social-social-media-interface-line-adri-ansyah-4.png',
         navigationName: 'Help',
-        link: ''
+        link: <PageInProduction />
     },
     {
         index: 'settings',
         inactiveImage: 'https://img.icons8.com/external-line-adri-ansyah/2x/external-social-social-media-interface-line-adri-ansyah-31.png',
         activeImage: 'https://img.icons8.com/external-line-adri-ansyah/2x/fb5012/external-social-social-media-interface-line-adri-ansyah-31.png',
         navigationName: 'Settings',
-        link: ''
+        link: <PageInProduction />
     },
 ]
 
@@ -61,48 +62,48 @@ export const CLIENTNAVIGATION = [
         inactiveImage: 'https://img.icons8.com/material-rounded/452/dashboard-layout.png',
         activeImage: 'https://img.icons8.com/material-rounded/452/fb5012/dashboard-layout.png',
         navigationName: 'Dashboard',
-        link: <ExpertDashboard />
+        link: <PageInProduction />
     },
     {
         index: 'profile',
         inactiveImage: 'https://img.icons8.com/external-becris-lineal-becris/344/external-user-mintab-for-ios-becris-lineal-becris.png',
         activeImage: 'https://img.icons8.com/external-becris-lineal-becris/344/fb5012/external-user-mintab-for-ios-becris-lineal-becris.png',
         navigationName: 'Profile',
-        link: ''
+        link: <PageInProduction />
     },
     {
         index: 'hiredgigs',
         inactiveImage: 'https://img.icons8.com/ios/2x/yandex-money.png',
         activeImage: 'https://img.icons8.com/ios/2x/fb5012/yandex-money.png',
         navigationName: 'Hired Gigs',
-        link: ''
+        link: <PageInProduction />
     },
     {
         index: 'messages',
         inactiveImage: 'https://img.icons8.com/external-outline-stroke-bomsymbols-/2x/external-chat-communication-outline-outline-stroke-bomsymbols--6.png',
         activeImage: 'https://img.icons8.com/external-outline-stroke-bomsymbols-/2x/fb5012/external-chat-communication-outline-outline-stroke-bomsymbols--6.png',
         navigationName: 'Messages',
-        link: ''
+        link: <PageInProduction />
     },
     {
         index: 'transactions',
         inactiveImage: 'https://img.icons8.com/external-outline-satawat-anukul/2x/external-economy-economy-outline-outline-satawat-anukul.png',
         activeImage: 'https://img.icons8.com/external-outline-satawat-anukul/2x/fb5012/external-economy-economy-outline-outline-satawat-anukul.png',
         navigationName: 'Transactions',
-        link: ''
+        link: <PageInProduction />
     },
     {
         index: 'help',
         inactiveImage: 'https://img.icons8.com/external-line-adri-ansyah/2x/external-social-social-media-interface-line-adri-ansyah-4.png',
         activeImage: 'https://img.icons8.com/external-line-adri-ansyah/2x/fb5012/external-social-social-media-interface-line-adri-ansyah-4.png',
         navigationName: 'Help',
-        link: ''
+        link: <PageInProduction />
     },
     {
         index: 'settings',
         inactiveImage: 'https://img.icons8.com/external-line-adri-ansyah/2x/external-social-social-media-interface-line-adri-ansyah-31.png',
         activeImage: 'https://img.icons8.com/external-line-adri-ansyah/2x/fb5012/external-social-social-media-interface-line-adri-ansyah-31.png',
         navigationName: 'Settings',
-        link: ''
+        link: <PageInProduction />
     },
 ]
---
+++ ge-web/src/pages/PageInProduction.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles/PageInProduction.css'
+
+const PageInProduction = () => {
+  return (
+    <div className='Body'>
+    <div className="Body_Components">
+   <div className="Production_Page">
+   <img src="images/production.png" alt="" srcset="" />
+    <h1> Page in Production</h1>
+   </div>
+    </div>
+    </div>
+  )
+}
+
+export default PageInProduction
